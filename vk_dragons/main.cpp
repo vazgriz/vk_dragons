c184c9e8dd24dedfaeca7d824a1c5e3519bb2bf8
vazgriz <vazgriz@gmail.com>
2017-07-26 15:16:00 -0500

    Add glfw3

--- vk_dragons/main.cpp
+++ vk_dragons/main.cpp
@@ -1,3 +1,20 @@
+#include<GLFW/glfw3.h>
+
+#define INITIAL_SIZE_WIDTH 800
+#define INITIAL_SIZE_HEIGHT 600
+
 int main() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(INITIAL_SIZE_WIDTH, INITIAL_SIZE_HEIGHT, "Here Be Dragons", nullptr, nullptr);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
