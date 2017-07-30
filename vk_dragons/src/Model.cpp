00f129116d5934802974cc8dab0bca3a63b24590
vazgriz <vazgriz@gmail.com>
2017-07-30 16:23:38 -0500

    Implement staging buffer transfer

--- vk_dragons/src/Model.cpp
+++ vk_dragons/src/Model.cpp
@@ -5,12 +5,21 @@ Model::Model(Renderer& renderer) : renderer(renderer) {
 }
 
 Model::~Model() {
-	vkDestroyBuffer(renderer.device, positionsBuffer, nullptr);
-	vkDestroyBuffer(renderer.device, normalsBuffer, nullptr);
-	vkDestroyBuffer(renderer.device, tangentsBuffer, nullptr);
-	vkDestroyBuffer(renderer.device, binormalsBuffer, nullptr);
-	vkDestroyBuffer(renderer.device, texcoordsBuffer, nullptr);
-	vkDestroyBuffer(renderer.device, indicesBuffer, nullptr);
+	vkDestroyBuffer(renderer.device, positionsBuffer.buffer, nullptr);
+	vkDestroyBuffer(renderer.device, normalsBuffer.buffer, nullptr);
+	vkDestroyBuffer(renderer.device, tangentsBuffer.buffer, nullptr);
+	vkDestroyBuffer(renderer.device, binormalsBuffer.buffer, nullptr);
+	vkDestroyBuffer(renderer.device, texcoordsBuffer.buffer, nullptr);
+	vkDestroyBuffer(renderer.device, indicesBuffer.buffer, nullptr);
+}
+
+void Model::DestroyStaging() {
+	vkDestroyBuffer(renderer.device, positionsStagingBuffer.buffer, nullptr);
+	vkDestroyBuffer(renderer.device, normalsStagingBuffer.buffer, nullptr);
+	vkDestroyBuffer(renderer.device, tangentsStagingBuffer.buffer, nullptr);
+	vkDestroyBuffer(renderer.device, binormalsStagingBuffer.buffer, nullptr);
+	vkDestroyBuffer(renderer.device, texcoordsStagingBuffer.buffer, nullptr);
+	vkDestroyBuffer(renderer.device, indicesStagingBuffer.buffer, nullptr);
 }
 
 void Model::Init(const std::string& fileName) {
@@ -47,7 +56,7 @@ void Model::CreateBuffers() {
 		*renderer.memory->deviceAllocator);
 }
 
-VkBuffer Model::CreateBuffer(VkDeviceSize size, VkBufferUsageFlags usage, Allocator allocator) {
+Buffer Model::CreateBuffer(VkDeviceSize size, VkBufferUsageFlags usage, Allocator allocator) {
 	VkBufferCreateInfo bufferInfo = {};
 	bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
 	bufferInfo.size = size;
@@ -67,5 +76,29 @@ VkBuffer Model::CreateBuffer(VkDeviceSize size, VkBufferUsageFlags usage, Alloca
 
 	vkBindBufferMemory(renderer.device, buffer, alloc.memory, alloc.offset);
 
+	return { buffer, alloc.size, alloc.offset };
+}
+
+Buffer Model::CopyBuffer(VkCommandBuffer commandBuffer, VkBuffer destBuffer, void* source, size_t size) {
+	Buffer buffer = CreateBuffer(size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, *renderer.memory->hostAllocator);
+	char* dest = static_cast<char*>(renderer.memory->hostMapping) + buffer.offset;
+	memcpy(dest, source, size);
+
+	VkBufferCopy copy = {};
+	copy.size = size;
+	copy.dstOffset = 0;	//these two offset values refer to the offset within each buffer, not the buffer's offset in memory
+	copy.srcOffset = 0;
+
+	vkCmdCopyBuffer(commandBuffer, buffer.buffer, destBuffer, 1, &copy);
+
 	return buffer;
+}
+
+void Model::UploadData(VkCommandBuffer commandBuffer) {
+	positionsStagingBuffer = CopyBuffer(commandBuffer, positionsBuffer.buffer, mesh.positions.data(), mesh.positions.size() * sizeof(glm::vec3));
+	normalsStagingBuffer = CopyBuffer(commandBuffer, normalsBuffer.buffer, mesh.normals.data(), mesh.normals.size() * sizeof(glm::vec3));
+	tangentsStagingBuffer = CopyBuffer(commandBuffer, tangentsBuffer.buffer, mesh.tangents.data(), mesh.tangents.size() * sizeof(glm::vec3));
+	binormalsStagingBuffer = CopyBuffer(commandBuffer, binormalsBuffer.buffer, mesh.binormals.data(), mesh.binormals.size() * sizeof(glm::vec3));
+	texcoordsStagingBuffer = CopyBuffer(commandBuffer, texcoordsBuffer.buffer, mesh.texcoords.data(), mesh.texcoords.size() * sizeof(glm::vec2));
+	indicesStagingBuffer = CopyBuffer(commandBuffer, indicesBuffer.buffer, mesh.indices.data(), mesh.indices.size() * sizeof(uint32_t));
 }
--- vk_dragons/src/Model.h
+++ vk_dragons/src/Model.h
@@ -6,22 +6,38 @@
 #include "MemorySystem.h"
 #include "Allocator.h"
 
+struct Buffer {
+	VkBuffer buffer;
+	size_t size;
+	size_t offset;
+};
+
 class Model {
 public:
 	Model(Renderer& renderer);
 	~Model();
 	void Init(const std::string& fileName);
+	void UploadData(VkCommandBuffer commandBuffer);
+	void DestroyStaging();
 
 private:
 	Renderer& renderer;
 	mesh_t mesh;
-	VkBuffer positionsBuffer;
-	VkBuffer normalsBuffer;
-	VkBuffer tangentsBuffer;
-	VkBuffer binormalsBuffer;
-	VkBuffer texcoordsBuffer;
-	VkBuffer indicesBuffer;
+	Buffer positionsBuffer;
+	Buffer normalsBuffer;
+	Buffer tangentsBuffer;
+	Buffer binormalsBuffer;
+	Buffer texcoordsBuffer;
+	Buffer indicesBuffer;
+
+	Buffer positionsStagingBuffer;
+	Buffer normalsStagingBuffer;
+	Buffer tangentsStagingBuffer;
+	Buffer binormalsStagingBuffer;
+	Buffer texcoordsStagingBuffer;
+	Buffer indicesStagingBuffer;
 
 	void CreateBuffers();
-	VkBuffer CreateBuffer(VkDeviceSize size, VkBufferUsageFlags usage, Allocator allocator);
+	Buffer CreateBuffer(VkDeviceSize size, VkBufferUsageFlags usage, Allocator allocator);
+	Buffer CopyBuffer(VkCommandBuffer commandBuffer, VkBuffer destBuffer, void* source, size_t size);
 };
