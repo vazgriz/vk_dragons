5d24be569a5140576c70df55242a658ba352e1c9
vazgriz <vazgriz@gmail.com>
2017-07-31 17:07:24 -0500

    Hide window until resources are loaded

--- vk_dragons/src/main.cpp
+++ vk_dragons/src/main.cpp
@@ -22,6 +22,7 @@ int main() {
 
 	//we don't need an OpenGL context, so specify GLFW_NO_API
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_VISIBLE, 0);
 	GLFWwindow* window = glfwCreateWindow(INITIAL_SIZE_WIDTH, INITIAL_SIZE_HEIGHT, "Here Be Dragons", nullptr, nullptr);
 
 	int _width, _height;
@@ -32,6 +33,8 @@ int main() {
 	glfwSetFramebufferSizeCallback(window, OnFramebufferResized);
 
 	Scene scene(window, width, height);
+
+	glfwShowWindow(window);
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
