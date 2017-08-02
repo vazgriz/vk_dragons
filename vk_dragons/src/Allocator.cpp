3a1903686ccee8968be23101be5aff3aae3768e8
vazgriz <vazgriz@gmail.com>
2017-08-02 17:25:43 -0500

    Refactor Allocator into a stack allocator

--- vk_dragons/src/Allocator.cpp
+++ vk_dragons/src/Allocator.cpp
@@ -7,6 +7,7 @@ Allocator::Allocator(VkDeviceMemory memory, size_t totalSize) {
 }
 
 Allocation Allocator::Alloc(size_t size, size_t alignment) {
+	stack.push(pointer);
 	//move pointer to requested alignment
 	size_t unalign = pointer % alignment;
 	if (unalign != 0) {
@@ -24,6 +25,12 @@ Allocation Allocator::Alloc(size_t size, size_t alignment) {
 	return result;
 }
 
+void Allocator::Free(Allocation allocation) {
+	pointer = stack.top();
+	stack.pop();
+}
+
 void Allocator::Reset() {
 	pointer = 0;
+	while (!stack.empty()) stack.pop();
 }
--- vk_dragons/src/Allocator.h
+++ vk_dragons/src/Allocator.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <stack>
 
 struct Allocation {
 	VkDeviceMemory memory;
@@ -9,14 +10,18 @@ struct Allocation {
 };
 
 class Allocator {
-	//simple linear allocator
+	//simple stack allocator
 public:
 	Allocator(VkDeviceMemory memory, size_t totalSize);
 	Allocation Alloc(size_t size, size_t alignment);
+	void Free(Allocation allocation);
 	void Reset();
 
 private:
 	VkDeviceMemory memory;
 	size_t totalSize;
 	size_t pointer;
+
+	//keeps track of where each allocation starts before alignment is applied, to prevent memory leaks
+	std::stack<size_t> stack;
 };
