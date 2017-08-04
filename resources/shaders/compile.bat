330156c86be569e91676df1079d69dd2a2eaf676
vazgriz <vazgriz@gmail.com>
2017-08-03 20:43:54 -0500

    Make compile script use env var

--- resources/shaders/compile.bat
+++ resources/shaders/compile.bat
@@ -1,3 +1,3 @@
-E:/VulkanSDK/1.0.17.0/Bin32/glslangValidator.exe -V prog1.vert -o prog1.vert.spv
-E:/VulkanSDK/1.0.17.0/Bin32/glslangValidator.exe -V prog1.frag -o prog1.frag.spv
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V prog1.vert -o prog1.vert.spv
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V prog1.frag -o prog1.frag.spv
 pause
