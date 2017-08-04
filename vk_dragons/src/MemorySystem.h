e819d2930a61d762efd8517a25bf981f04f026d6
vazgriz <vazgriz@gmail.com>
2017-08-03 21:30:18 -0500

    Update comments

--- vk_dragons/src/MemorySystem.h
+++ vk_dragons/src/MemorySystem.h
@@ -12,8 +12,8 @@ public:
 	Memory(VkPhysicalDevice physicalDevice, VkDevice device);
 	void Cleanup();
 
-	//linear allocators are used because this application has simple memory requirements
-	//everything gets uploaded to the gpu at start up and nothing needs to be uploaded after that
+	//stack allocators are used because this application has simple memory requirements
+	//every resource gets allocated at start up and only full screen buffers need to be allocated after that
 	//larger applications will have to use more sophisticated allocation schemes
 	std::unique_ptr<Allocator> hostAllocator;
 	std::unique_ptr<Allocator> deviceAllocator;
