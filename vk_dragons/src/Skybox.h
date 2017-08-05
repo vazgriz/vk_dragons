e20fd65b1679fc229d7bc69951d4245e95522990
vazgriz <vazgriz@gmail.com>
2017-08-05 17:20:12 -0500

    Add Skybox class

---
+++ vk_dragons/src/Skybox.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Renderer.h"
+#include "ProgramUtilities.h"
+
+class Skybox {
+public:
+	Skybox(Renderer& renderr);
+	~Skybox();
+	void Init();
+	void UploadData(VkCommandBuffer commandBuffer);
+	void DestroyStaging();
+	void Draw(VkCommandBuffer commandBuffer);
+
+	static std::vector<VkVertexInputBindingDescription> GetBindingDescriptions();
+	static std::vector<VkVertexInputAttributeDescription> GetAttributeDescriptions();
+
+private:
+	Renderer& renderer;
+
+	Buffer vertexBuffer;
+	Buffer indexBuffer;
+	Buffer vertexStagingBuffer;
+	Buffer indexStagingBuffer;
+};
