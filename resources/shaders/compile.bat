34e77836a021f298e66dd2c6902ae0762d1197c3
vazgriz <vazgriz@gmail.com>
2017-08-06 22:19:36 -0500

    Add -s flag

--- resources/shaders/compile.bat
+++ resources/shaders/compile.bat
@@ -3,8 +3,8 @@
 %VK_SDK_PATH%/Bin32/glslangValidator.exe -V cube.vert -o cube.vert.spv -s
 %VK_SDK_PATH%/Bin32/glslangValidator.exe -V cube.frag -o cube.frag.spv -s
 %VK_SDK_PATH%/Bin32/glslangValidator.exe -V object_depth.vert -o object_depth.vert.spv -s
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V object.vert -o object.vert.spv
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V object.frag -o object.frag.spv
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V plane.vert -o plane.vert.spv
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V plane.frag -o plane.frag.spv
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V object.vert -o object.vert.spv -s
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V object.frag -o object.frag.spv -s
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V plane.vert -o plane.vert.spv -s
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V plane.frag -o plane.frag.spv -s
 pause
