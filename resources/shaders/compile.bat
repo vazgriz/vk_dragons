ce359d41c7dc98b635b09fe0bf103443672c1cf0
vazgriz <vazgriz@gmail.com>
2017-08-06 19:38:02 -0500

    Add object_depth shader to compile script

--- resources/shaders/compile.bat
+++ resources/shaders/compile.bat
@@ -2,4 +2,5 @@
 %VK_SDK_PATH%/Bin32/glslangValidator.exe -V prog1.frag -o prog1.frag.spv -s
 %VK_SDK_PATH%/Bin32/glslangValidator.exe -V cube.vert -o cube.vert.spv -s
 %VK_SDK_PATH%/Bin32/glslangValidator.exe -V cube.frag -o cube.frag.spv -s
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V object_depth.vert -o object_depth.vert.spv -s
 pause
