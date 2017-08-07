7a6bb517fd30520e348bcc70459a6fdf88f59a91
vazgriz <vazgriz@gmail.com>
2017-08-06 22:37:29 -0500

    Make uniform accessible from fragment shader

--- vk_dragons/src/Scene.cpp
+++ vk_dragons/src/Scene.cpp
@@ -429,7 +429,7 @@ void Scene::CreateUniformSetLayout() {
 	uboLayoutBinding.binding = 0;
 	uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 	uboLayoutBinding.descriptorCount = 1;
-	uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+	uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
 
 	VkDescriptorSetLayoutCreateInfo layoutInfo = {};
 	layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
