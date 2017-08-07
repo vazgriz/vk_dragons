787b0f95cc00c2d2f0ecb49b5acdc9093685062f
vazgriz <vazgriz@gmail.com>
2017-08-07 13:59:11 -0500

    Reformat compile script

--- resources/shaders/compile.bat
+++ resources/shaders/compile.bat
@@ -1,10 +1,10 @@
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V prog1.vert -o prog1.vert.spv -s
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V prog1.frag -o prog1.frag.spv -s
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V cube.vert -o cube.vert.spv -s
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V cube.frag -o cube.frag.spv -s
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V object_depth.vert -o object_depth.vert.spv -s
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V object.vert -o object.vert.spv -s
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V object.frag -o object.frag.spv -s
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V plane.vert -o plane.vert.spv -s
-%VK_SDK_PATH%/Bin32/glslangValidator.exe -V plane.frag -o plane.frag.spv -s
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V -s prog1.vert -o prog1.vert.spv
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V -s prog1.frag -o prog1.frag.spv
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V -s cube.vert -o cube.vert.spv
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V -s cube.frag -o cube.frag.spv
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V -s object_depth.vert -o object_depth.vert.spv
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V -s object.vert -o object.vert.spv
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V -s object.frag -o object.frag.spv
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V -s plane.vert -o plane.vert.spv
+%VK_SDK_PATH%/Bin32/glslangValidator.exe -V -s plane.frag -o plane.frag.spv
 pause
