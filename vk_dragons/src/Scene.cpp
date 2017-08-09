3464e0a7c1c1ccdb1219ba6ded66aabdd5f5cc5c
vazgriz <vazgriz@gmail.com>
2017-08-09 17:46:06 -0500

    Fix box blur render pass

--- vk_dragons/src/Scene.cpp
+++ vk_dragons/src/Scene.cpp
@@ -456,7 +456,7 @@ void Scene::CreateBoxBlurRenderPass() {
 	colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
 	colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
 	colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-	colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+	colorAttachment.finalLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
 
 	VkAttachmentReference colorAttachmentRef = {};
 	colorAttachmentRef.attachment = 0;
