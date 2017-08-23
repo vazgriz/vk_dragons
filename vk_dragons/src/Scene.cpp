55c4232da71f4a557093b614a1c170c80f1aaaad
vazgriz <vazgriz@gmail.com>
2017-08-23 12:07:08 -0500

    Change command buffer usage

--- vk_dragons/src/Scene.cpp
+++ vk_dragons/src/Scene.cpp
@@ -234,7 +234,7 @@ void Scene::RecordCommandBuffer(uint32_t imageIndex) {
 
 	VkCommandBufferBeginInfo beginInfo = {};
 	beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-	beginInfo.flags = VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT;
+	beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
 
 	vkBeginCommandBuffer(commandBuffer, &beginInfo);
 
