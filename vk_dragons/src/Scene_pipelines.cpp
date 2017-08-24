ec9a8811d725362deb58e2ea14ca5145a1ade14e
vazgriz <vazgriz@gmail.com>
2017-08-23 22:01:36 -0500

    Render only back faces in depth pass

--- vk_dragons/src/Scene_pipelines.cpp
+++ vk_dragons/src/Scene_pipelines.cpp
@@ -461,7 +461,7 @@ void Scene::CreateLightPipeline() {
 	rasterizer.rasterizerDiscardEnable = VK_FALSE;
 	rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
 	rasterizer.lineWidth = 1.0f;
-	rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+	rasterizer.cullMode = VK_CULL_MODE_FRONT_BIT;
 	rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
 	rasterizer.depthBiasEnable = VK_FALSE;
 
